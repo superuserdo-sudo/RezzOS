9f8b30a3e3cebb308c71ab21cc5071789ee662c3
superuserdo-sudo <tfstcddgvdrt@gmail.com>
2026-08-31 16:45:28 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,18 +11,20 @@ EXTRA_DRIVE=""
 if [ -f "target-disk.img" ]; then
     EXTRA_DRIVE="-drive file=target-disk.img,format=raw,if=virtio"
 fi
-
-qemu-system-x86_64 \
+ENABLE_KVM=""
+if [ -e /dev/kvm ]; then
+    ENABLE_KVM="-enable-kvm -cpu host"
+fi
+exec qemu-system-x86_64 \
+    $ENABLE_KVM \
     -kernel bzImage \
     -initrd rootfs.cpio.gz \
-    -append "console=ttyS0 resume=/dev/vda fbcon=font:$FONT random.trust_cpu=on" \
+    -append "console=ttyS0 quiet fbcon=font:$FONT random.trust_cpu=on" \
     -netdev user,id=net0,hostfwd=tcp::2222-:22 -device virtio-net,netdev=net0 \
     -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0 \
     -drive file=disk.img,format=raw,if=virtio \
     $EXTRA_DRIVE \
     -m 1024M -nographic \
-    -enable-kvm \
-    -cpu host \
     -usb \
     -device usb-tablet \
     -device usb-kbd 
